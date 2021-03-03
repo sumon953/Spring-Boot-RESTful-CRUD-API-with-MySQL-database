90cea729061abf1b27ce5f5a5e73a67a87147630
43031 <sumonsorder953@gmail.com>
2021-03-03 14:34:57 +0600

    First

---
+++ src/main/java/com/example/Spring/SpringBootResTfulCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResTfulCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResTfulCrudApiApplication.class, args);
+	}
+
+}
